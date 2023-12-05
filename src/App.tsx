352a71dfa827ff3b2b99a4b5b3948b14790e789e
dvornicked <dvornicked@yandex.ru>
2023-12-06 01:36:12 +0300

    Implement beforeinstallprompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {Card, CardHeader, CardTitle, CardDescription} from "@/components/ui/card";
+import {Button} from "@/components/ui/button.tsx";
 
 interface Todo {
     userId: number;
@@ -8,8 +9,19 @@ interface Todo {
     completed: boolean;
 }
 
+export interface IBeforeInstallPromptEvent extends Event {
+    readonly platforms: Array<string>;
+    readonly userChoice: Promise<{
+        outcome: 'accepted' | 'dismissed',
+        platform: string
+    }>;
+
+    prompt(): Promise<void>;
+}
+
 function App() {
     const [todos, setTodos] = useState<Todo[]>([])
+    const [deferredPrompt, setDeferredPrompt] = useState<IBeforeInstallPromptEvent | null>(null)
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/todos")
@@ -17,11 +29,28 @@ function App() {
             .then((todos) => setTodos(todos));
     }, []);
 
+    useEffect(() => {
+        const handler = (e: Event) => {
+            e.preventDefault()
+            setDeferredPrompt(e as IBeforeInstallPromptEvent)
+        }
+
+        window.addEventListener("beforeinstallprompt", handler)
+        return () => window.removeEventListener("beforeinstallprompt", handler)
+    }, [])
+
+    const handleInstall = useCallback(() => {
+        if (!deferredPrompt) return
+        void deferredPrompt.prompt()
+        deferredPrompt.userChoice.then(console.log)
+    }, [deferredPrompt])
+
     return (
         <>
             <h1 className="text-4xl font-extrabold text-center">
                 Todos-PWA
             </h1>
+            <Button className="mt-4 mx-auto max-w-2xl block" onClick={handleInstall}>Install</Button>
             <ul className="space-y-4 mt-4 max-w-2xl mx-auto">
                 {todos.map((todo) => {
                     return (
